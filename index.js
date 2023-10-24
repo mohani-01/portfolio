dd5ee84ec2f5a8809e944dda1ec1d1d390fd169f
Nahom <nahomohani@gmail.com>
2023-10-24 11:22:46 +0300

    project animation is going well

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Hello")
-    document.querySelector('.navbar').addEventListener('click', () =>   {
-        console.log("nav")
+document.addEventListener('DOMContentLoaded', () => {
+
+    document.querySelectorAll(".description").forEach(div => {
+
+        div.addEventListener('mouseover', () => {
+            const see = div.querySelector('.see');
+            const parent = div.parentElement;
+            parent.addEventListener('animationend', () => {
+                see.style.display = 'block';
+            })
+
+
+
+
+        })
+        div.addEventListener('mouseout', () => {
+            const see = div.querySelector('.see');
+            const parent = div.parentElement;
+            parent.addEventListener('transitionend', () => {
+
+                see.style.display = 'none';
+            })
+        })
     })
 
+
+
+    document.addEventListener('scrollend', () => {
+    //    document.querySelector('body').style.backgroundColor = '';
+    })
 })
+
+
