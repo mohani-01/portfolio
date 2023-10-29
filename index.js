357904ffccfbcf0053d7d58dc80075a3d720b627
Nahom <nahomohani@gmail.com>
2023-10-29 08:41:13 +0300

    add sololearn certificate's and navbar bootstrap

--- index.js
+++ index.js
@@ -1,101 +1,101 @@
-document.addEventListener('DOMContentLoaded', () => {
-   const BEFORE = document.getElementById('before')
-   const AFTER = document.getElementById('after')
-   const LANG = document.getElementById('lang')
+// document.addEventListener('DOMContentLoaded', () => {
+//    const BEFORE = document.getElementById('before')
+//    const AFTER = document.getElementById('after')
+//    const LANG = document.getElementById('lang')
    
-   let words = {'Javascript': {
-               "before": 'console.log("',
-               "after" : '");'
-                },
-                "Python": {
-                    "before": 'print("',
-                    "after": '")'
-                },
-                "C" : {
-                    "before": 'printf("',
-                    "after" : '");'
-                },
-                "C#" : {
-                    "before": 'Console.WriteLine("',
-                    "after" : '"):'
-                },
-                "Java": {
-                    "before": 'System.out.println("',
-                    "after" :'");'
-                },
-                "PHP": {
-                    "before": 'echo "',
-                    "after" : '";'
-                }
-    }
+//    let words = {'Javascript': {
+//                "before": 'console.log("',
+//                "after" : '");'
+//                 },
+//                 "Python": {
+//                     "before": 'print("',
+//                     "after": '")'
+//                 },
+//                 "C" : {
+//                     "before": 'printf("',
+//                     "after" : '");'
+//                 },
+//                 "C#" : {
+//                     "before": 'Console.WriteLine("',
+//                     "after" : '"):'
+//                 },
+//                 "Java": {
+//                     "before": 'System.out.println("',
+//                     "after" :'");'
+//                 },
+//                 "PHP": {
+//                     "before": 'echo "',
+//                     "after" : '";'
+//                 }
+//     }
    
 
-    const keys = Object.keys(words)
-    const change = setInterval(() => {
-        let index = Math.ceil(Math.random() * keys.length) - 1 ;
-        console.log(index)
-        AFTER.innerHTML = words[keys[index]].after;
-        LANG.innerHTML = keys[index];
+//     const keys = Object.keys(words)
+//     const change = setInterval(() => {
+//         let index = Math.ceil(Math.random() * keys.length) - 1 ;
+//         console.log(index)
+//         AFTER.innerHTML = words[keys[index]].after;
+//         LANG.innerHTML = keys[index];
 
-        document.getElementById('hello_world').style.color = "#" + Math.floor(Math. random()*16777215).toString(16);
-        console.log(Math.floor(Math. random()*16777215));
-        BEFORE.innerHTML = words[keys[index]].before;
-            // setTimeout(() => {
+//         document.getElementById('hello_world').style.color = "#" + Math.floor(Math. random()*16777215).toString(16);
+//         console.log(Math.floor(Math. random()*16777215));
+//         BEFORE.innerHTML = words[keys[index]].before;
+//             // setTimeout(() => {
 
-            // }
-        // ,500)
-    }, 2000)
+//             // }
+//         // ,500)
+//     }, 2000)
 
-    // for(let key in words) {
-    //     if (words.hasOwnProperty(key)) {
-    //         console.log(keys)
-    //     }
-    // }
-    // for (let i = 0, N = key.length; i < N; i++) {
-    //     console.log(key, words[newkey], words[key[i]])
+//     // for(let key in words) {
+//     //     if (words.hasOwnProperty(key)) {
+//     //         console.log(keys)
+//     //     }
+//     // }
+//     // for (let i = 0, N = key.length; i < N; i++) {
+//     //     console.log(key, words[newkey], words[key[i]])
 
         
-    // }
+//     // }
    
    
    
    
    
-    document.querySelectorAll('.see').forEach(div => {
-        div.style.display  = 'none';
-        console.log(div)
-    })
+//     document.querySelectorAll('.see').forEach(div => {
+//         div.style.display  = 'none';
+//         console.log(div)
+//     })
 
-    document.querySelectorAll(".pro").forEach(div => {
+//     document.querySelectorAll(".pro").forEach(div => {
 
-        div.addEventListener('mouseover', () => {
-            const see = div.querySelector('.see');
-            const parent = div.parentElement;
-            div.querySelector('.description').addEventListener('mouseover', () => {
+//         div.addEventListener('mouseover', () => {
+//             const see = div.querySelector('.see');
+//             const parent = div.parentElement;
+//             div.querySelector('.description').addEventListener('mouseover', () => {
 
-                see.style.display = 'flex';
-                see.style.opacity = 1;  
-            })
-            // })
-
-
-        })
-        div.parentElement.addEventListener('mouseout', () => {
-            const see = div.querySelector('.see');
-            const parent = div.parentElement;
-            div.querySelector('.description').addEventListener('mouseout', () => {
-                
-                see.style.display = 'none';
-                see.style.opacity = 0;
-            })
-        })
-    })
+//                 see.style.display = 'flex';
+//                 see.style.opacity = 1;  
+//             })
+//             // })
 
 
+//         })
+//         div.parentElement.addEventListener('mouseout', () => {
+//             const see = div.querySelector('.see');
+//             const parent = div.parentElement;
+//             div.querySelector('.description').addEventListener('mouseout', () => {
+//                 // 
+//                 see.style.display = 'none';
+//                 see.style.opacity = 0;
+//             })
+//         })
+//     })
 
-    document.addEventListener('scrollend', () => {
-    //    document.querySelector('body').style.backgroundColor = '';
-    })
-})
+
+
+//     document.addEventListener('scrollend', () => {
+//     //    document.querySelector('body').style.backgroundColor = '';
+//     })
+// })
 
 
