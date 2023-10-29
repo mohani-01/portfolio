fe54a0b6015cfd88f60f6d76f19ca3bece77f71e
Nahom <nahomohani@gmail.com>
2023-10-29 21:01:19 +0300

    Nice cs50's certifcate, project display, and Javascript

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
 //    const BEFORE = document.getElementById('before')
 //    const AFTER = document.getElementById('after')
 //    const LANG = document.getElementById('lang')
@@ -96,6 +96,56 @@
 //     document.addEventListener('scrollend', () => {
 //     //    document.querySelector('body').style.backgroundColor = '';
 //     })
-// })
 
+    
+    // document.querySelectorAll('svg').forEach(svg => { 
+    //     svg.addEventListener('transitionend', () => {
+    //         svg.style.transform = 'none';
+    //     svg.addEventListener('mouseover', () => {
+        
+    //         if (svg.style.transform = 'none') {
+    //             svg.style.transform = 'rotate('+ 15+ 'deg)';
+    //         } 
+    //     })
+    //     })
+        
+        
+
+
+    // });
+
+    document.querySelectorAll('section').forEach(section => {
+        console.log(section.id, section.getBoundingClientRect().top)
+        console.log(section.id, section.getBoundingClientRect().height + section.getBoundingClientRect().top)
+        console.log(window.scrollY)
+        console.log(window.innerHeight)
+    })
+    window.onscroll = () => {
+        console.log(window.innerHeight)
+        document.querySelectorAll('section').forEach(section => {
+            const scrollHeight = Math.floor(window.scrollY - 84)
+            const bottom = Math.floor(section.getBoundingClientRect().height + section.getBoundingClientRect().top + window.innerHeight) ;
+            if ( Math.floor(section.getBoundingClientRect().top + scrollHeight) < Math.floor(scrollHeight) && (Math.floor(bottom)  > Math.floor(scrollHeight) )) {
+                changeTop(section)
+            }
+            if (section.id === "Contact-Me") {
+                console.log("top", section.getBoundingClientRect().top + window.scrollY, window.scrollY)
+
+            }
+            // console.log("bounding element", section,    section.getBoundingClientRect().y)
+        //    console.log(section.getBoundingClientRect().top)
+        })
+    }
+})
+
+
+
+function changeTop(currentSection) {
+    console.log("top", currentSection.getBoundingClientRect().y, window.scrollY)
+    console.log("bottom", currentSection.getBoundingClientRect().bottom + window.innerHeight, window.scrollY)
+    document.querySelectorAll('section').forEach(section => {
+        section.className = "";
+    })
+    currentSection.className = "border";
+}
 
