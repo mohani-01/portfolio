04c8ddac4bca1f4796222695c8de4d24fd973464
Nahom <nahomohani@gmail.com>
2023-11-04 11:47:15 +0300

    fetch contact me message and nav-bar collapse id done

--- index.js
+++ index.js
@@ -1,119 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {
-    changeSection();
+let scroll = true;
+
+changeSection();
 //    const BEFORE = document.getElementById('before')
 //    const AFTER = document.getElementById('after')
 //    const LANG = document.getElementById('lang')
-   
-//    let words = {'Javascript': {
-//                "before": 'console.log("',
-//                "after" : '");'
-//                 },
-//                 "Python": {
-//                     "before": 'print("',
-//                     "after": '")'
-//                 },
-//                 "C" : {
-//                     "before": 'printf("',
-//                     "after" : '");'
-//                 },
-//                 "C#" : {
-//                     "before": 'Console.WriteLine("',
-//                     "after" : '"):'
-//                 },
-//                 "Java": {
-//                     "before": 'System.out.println("',
-//                     "after" :'");'
-//                 },
-//                 "PHP": {
-//                     "before": 'echo "',
-//                     "after" : '";'
-//                 }
-//     }
-   
 
-//     const keys = Object.keys(words)
-//     const change = setInterval(() => {
-//         let index = Math.ceil(Math.random() * keys.length) - 1 ;
-//         console.log(index)
-//         AFTER.innerHTML = words[keys[index]].after;
-//         LANG.innerHTML = keys[index];
+document.querySelectorAll('.see').forEach(div => {
+    const hyperlink = div.querySelectorAll('a')
 
-//         document.getElementById('hello_world').style.color = "#" + Math.floor(Math. random()*16777215).toString(16);
-//         console.log(Math.floor(Math. random()*16777215));
-//         BEFORE.innerHTML = words[keys[index]].before;
-//             // setTimeout(() => {
-
-//             // }
-//         // ,500)
-//     }, 2000)
-
-//     // for(let key in words) {
-//     //     if (words.hasOwnProperty(key)) {
-//     //         console.log(keys)
-//     //     }
-//     // }
-//     // for (let i = 0, N = key.length; i < N; i++) {
-//     //     console.log(key, words[newkey], words[key[i]])
-
+    hyperlink.forEach(element => {
         
-//     // }
-   
-   
-   
-   
-   
-//     document.querySelectorAll('.see').forEach(div => {
-//         div.style.display  = 'none';
-//         console.log(div)
-//     })
-
-//     document.querySelectorAll(".pro").forEach(div => {
-
-//         div.addEventListener('mouseover', () => {
-//             const see = div.querySelector('.see');
-//             const parent = div.parentElement;
-//             div.querySelector('.description').addEventListener('mouseover', () => {
-
-//                 see.style.display = 'flex';
-//                 see.style.opacity = 1;  
-//             })
-//             // })
-
-
-//         })
-//         div.parentElement.addEventListener('mouseout', () => {
-//             const see = div.querySelector('.see');
-//             const parent = div.parentElement;
-//             div.querySelector('.description').addEventListener('mouseout', () => {
-//                 // 
-//                 see.style.display = 'none';
-//                 see.style.opacity = 0;
-//             })
-//         })
-//     })
-
-
-
-//     document.addEventListener('scrollend', () => {
-//     //    document.querySelector('body').style.backgroundColor = '';
-//     })
-
+        element.addEventListener("click", (event) => {
     
-    // document.querySelectorAll('svg').forEach(svg => { 
-    //     svg.addEventListener('transitionend', () => {
-    //         svg.style.transform = 'none';
-    //     svg.addEventListener('mouseover', () => {
-        
-    //         if (svg.style.transform = 'none') {
-    //             svg.style.transform = 'rotate('+ 15+ 'deg)';
-    //         } 
-    //     })
-    //     })
-        
-        
-
-
-    // });
+            if (parseFloat(window.getComputedStyle(div).getPropertyValue('opacity')) < 0.8 ) {
+            
+                event.preventDefault()
+            }
+        })
+    });
+})
 
     document.querySelectorAll('section').forEach(section => {
         // console.log(section.id, section.getBoundingClientRect().top)
@@ -125,46 +30,51 @@ document.addEventListener('DOMContentLoaded', () => {
     const navbar = document.getElementById('navbarNav');
     navbar.querySelectorAll('a').forEach(ahref => {
         ahref.addEventListener('click', () => {
+            scroll = false
             navbar.querySelectorAll('a').forEach(event => {
                 event.style.borderBottomColor = 'transparent';
             })
             ahref.style.borderBottomColor = 'green';
+            window.addEventListener('scrollend', () => {
+                scroll = true
+            })
 
         })
     })
-    window.addEventListener('wheel', () => {
+    window.addEventListener('scroll', () => {
         changeSection()
        
     })
-})
+
+    window.addEventListener('scroll', () => {
+        // changeSection();
+    })
+
 
 
 
 function changeSection() {
-    const navbar = document.getElementById('navbarNav');
+    if (scroll ) {
+        const navbar = document.getElementById('navbarNav');
 
-    const section =  document.querySelector('#Projects');
-    const total = Math.floor(Math.abs(section.getBoundingClientRect().bottom) + Math.floor(Math.abs(section.getBoundingClientRect().top)));
-    const height = Math.floor(section.getBoundingClientRect().height);
-    console.log(total, height, total === height)
-//    console.log((Math.abs(section.getBoundingClientRect().bottom) + Math.floor(Math.abs(section.getBoundingClientRect().top))));
-        
-    // console.log((Math.abs(section.getBoundingClientRect().top) + Math.abs(section.getBoundingClientRect().bottom)),"Height", section.getBoundingClientRect().height, "Bottom>>",section.getBoundingClientRect().bottom, "Top>>>" ,(section.getBoundingClientRect().top), ">>", (section.getBoundingClientRect().height), window.scrollY);
-    // console.log('bottom', section.getBoundingClientRect().bottom)
-    // console.log(window.innerHeight)
-    document.querySelectorAll('section').forEach(section => {
-        const total = Math.floor(Math.abs(section.getBoundingClientRect().bottom) + (Math.abs(section.getBoundingClientRect().top)));
+        const section =  document.querySelector('#Projects');
+        const total = Math.floor(Math.abs(section.getBoundingClientRect().bottom) + Math.floor(Math.abs(section.getBoundingClientRect().top)));
         const height = Math.floor(section.getBoundingClientRect().height);
-        if (  total === height) {
-            console.log(section)
-            // console.log(section, total, height)
-            // console.log(section)
-            changeTop(section, navbar)
-        }
-      
-        // console.log("bounding element", section,    section.getBoundingClientRect().y)
-    //    console.log(section.getBoundingClientRect().top)
-    })
+        // console.log(total, height, total === height)
+    //    console.log((Math.abs(section.getBoundingClientRect().bottom) + Math.floor(Math.abs(section.getBoundingClientRect().top))));
+            
+        // console.log((Math.abs(section.getBoundingClientRect().top) + Math.abs(section.getBoundingClientRect().bottom)),"Height", section.getBoundingClientRect().height, "Bottom>>",section.getBoundingClientRect().bottom, "Top>>>" ,(section.getBoundingClientRect().top), ">>", (section.getBoundingClientRect().height), window.scrollY);
+        // console.log('bottom', section.getBoundingClientRect().bottom)
+        // console.log(window.innerHeight)
+        document.querySelectorAll('section').forEach(section => {
+            const total = Math.floor(Math.abs(section.getBoundingClientRect().bottom) + (Math.abs(section.getBoundingClientRect().top)));
+            const height = Math.floor(section.getBoundingClientRect().height);
+            if (  total === height) {
+                changeTop(section, navbar)
+            }
+        })
+    }
+   
 }
 
 function changeTop(currentSection, navbar) {
@@ -184,3 +94,93 @@ function changeTop(currentSection, navbar) {
     // currentSection.className = "border";
 }
 
+
+const toggler = document.getElementsByClassName('navbar-toggler')
+const toggle = document.querySelector('.navbar-toggler')
+console.log(toggler)
+console.log(toggle)
+
+console.log(toggle.getAttribute('aria-expanded'))
+const ulContainer = document.getElementsByClassName('navbar-nav')
+
+
+console.log(ulContainer)
+document.querySelector('.navbar-nav').querySelectorAll('a').forEach(a => {
+    a.addEventListener('click', (event) => {
+        if (window.getComputedStyle(toggle).display === 'none') {
+
+            console.log(event.target)
+        } else {
+            toggle.click()
+        }
+        // console.log(event.target, window.getComputedStyle(toggle).display)
+    })
+})
+
+document.getElementById('messageInput').addEventListener("input", (event) => {
+    const textarea = event.target;
+    textarea.style.height = 'auto';
+    textarea.style.height = textarea.scrollHeight + 24 + 'px';   
+    
+    
+})
+
+
+
+// document.getElementById('email-address').addEventListener('input', (event) => {
+//     const email = event.target.value;
+//     if (email.trim().length > 5) {
+//         console.log('listening')
+//         console.log(document.getElementById('floatText').value)
+//         if (document.getElementById('floatText').value){ 
+//             document.getElementById('send').disabled = false;
+//         }
+
+//     }
+// })
+
+
+
+
+// fetch('https://django-js-mail.onrender.com/emails', {
+//     method: 'POST',
+//     body: JSON.stringify({
+//       recipients: emailRecipients,
+//       subject: emailSubject,
+//       body: emailBody,
+//     })
+//   })      
+
+//   .then(response =>  response.json())
+//   .then(result => {
+
+
+
+document.getElementById('send').addEventListener('click', (event) => {
+    event.preventDefault()
+
+   // https://github.com/github/fetch
+    fetch("https://formsubmit.co/ajax/646613eabd667d1f253d05d759e2ebef", {
+        method: "POST",
+        headers: { 
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            email: document.getElementById('emailInput').value,
+            message: document.getElementById('messageInput').value
+        })
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+})
+
+
+
+
+{/* <form action="https://formsubmit.co/your@email.com" method="POST">
+     <input type="text" name="name" required>
+     <input type="email" name="email" required>
+     <button type="submit">Send</button>
+</form> */}
